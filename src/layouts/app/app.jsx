65ae75b16b2072234f80369764583c6c81f05014
allen <294931812@qq.com>
2017-12-13 02:32:25 +0800

    add demo

--- src/layouts/app/app.jsx
+++ src/layouts/app/app.jsx
@@ -8,14 +8,13 @@ class App extends React.Component {
         super(props);
         this.state = {
             headerImg: require('./images/header.jpg'),
-            logoImg: "https://ami-img.b0.upaiyun.com/activityNew/Indianamb/common/img-logo.png",
             bodyImg: require('./images/img-step1.jpg'),
             footerImg: APP.BROWSER.isclient ? require('./images/footer-app.jpg') : require('./images/footer-wechat.jpg')
         }
     }
 
     render() {
-        const {headerImg, logoImg, bodyImg, footerImg} = this.state;
+        const {headerImg, bodyImg, footerImg} = this.state;
         return <div className="app">
             <div className="header">
                 <img src={headerImg}/>
