c126047213b1d197c611e7c0ca7a0b67de24aceb
allen <294931812@qq.com>
2017-12-25 01:20:29 +0800

    add demo

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,8 @@
 /**
  * Created by Administrator on 2017/11/9.
  */
-module.exports = {
+
+module.exports = ({file, options, env}) => ({
     plugins: [
         require('autoprefixer'),
         require('postcss-pxtorem')({
@@ -9,4 +10,4 @@ module.exports = {
             propList: ['*', '!letter-spacing']
         })
     ]
-}
+})
