947ae920693eb130b8ef35d38c031cbf10cc4d2c
allen <294931812@qq.com>
2018-03-23 11:28:20 +0800

    add test

--- cfg/utils.js
+++ cfg/utils.js
@@ -2,7 +2,7 @@
  * Created by Administrator on 2017/12/10.
  */
 const path = require('path')
-const ifaces = require("os").networkInterfaces()
+const os = require("os")
 
 
 /**
@@ -14,12 +14,12 @@ exports.ip = getIp()
 function getIp() {
     var ip = "127.0.0.1";
 
-    for (var dev in ifaces) {
-        ifaces[dev].forEach(function (details) {
-            if (details.family == 'IPv4' && details.address !== '127.0.0.1') {
-                ip = details.address;
-            }
-        })
+    console.log(os.networkInterfaces())
+
+    for(var i=0;i<os.networkInterfaces().en0.length;i++){
+        if(os.networkInterfaces().en0[i].family=='IPv4'){
+            ip=os.networkInterfaces().en0[i].address;
+        }
     }
     return ip
 }
